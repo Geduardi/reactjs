e7ff60aa394d3dfeb4821e9c74a6d401ba982c1b
Geduardi <eduardilgen@gmail.com>
2022-04-14 00:28:25 +0200

    Сделано обязательное ДЗ 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import {Message} from "./components/Message/Message";
+import {useEffect, useState} from "react";
+import {Form} from "./components/Form/Form";
+
+const name = 'me';
+const robotName = 'Bot';
+
 
 function App() {
+    const [messageList,setMessageList] = useState([]);
+    const [lastAuthor, setLastAuthor] = useState('');
+
+    const addMsg = (inputText) => {
+        setMessageList([...messageList,{author: name, text: inputText}])
+        setLastAuthor(name);
+    }
+
+    useEffect(()=>{
+        if (messageList.length > 0 && lastAuthor !== robotName) {
+            setMessageList([...messageList,{author:robotName, text: "Ваше сообщение отправлено"}])
+            setLastAuthor(robotName);
+        }
+    },[messageList,lastAuthor])
+
   return (
     <div className="App">
-      <Message message_text="Message text"/>
+        <div className="messageArea">
+            {messageList.map((message)=>(<Message msgText={message.text} author={message.author} robotName={robotName} />))}
+        </div>
+        <Form onSubmit={addMsg} />
     </div>
   );
 }
